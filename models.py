582ef9210750d49c2225f1fc05bb72fa7e9c6a4f
Kyle Shainca Nicole V. Sambling <160310459+bling011@users.noreply.github.com>
2025-04-23 23:08:51 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,9 +1,8 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String
 from database import Base
 
-class Book(Base):
-    __tablename__ = "books"
-    
+class Todo(Base):
+    __tablename__ = "todos"
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
-    author = Column(String)
+    completed = Column(Boolean, default=False)
